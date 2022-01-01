dc25e08e91a0ecdcb3ee4550a482d6ba33ad505f
Minsuk Eom <boxresin@gmail.com>
2022-01-01 20:52:14 +0900

    실험 구성

--- app/src/main/java/kr/boxresin/experiment/viewrecycle/MainActivity.kt
+++ app/src/main/java/kr/boxresin/experiment/viewrecycle/MainActivity.kt
@@ -1,6 +1,7 @@
 package kr.boxresin.experiment.viewrecycle
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 /** 메인 액티비티 */
@@ -8,6 +9,15 @@ class MainActivity : AppCompatActivity() {
     /** 메인 액티비티가 생성될 때 호출된다. */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        if (contentView == null) {
+            contentView = layoutInflater.inflate(R.layout.activity_main, null)
+        }
+
+        setContentView(contentView)
+    }
+
+    companion object {
+        private var contentView: View? = null
     }
 }
