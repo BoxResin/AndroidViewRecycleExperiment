8c6d55663059fe59edaec1fcc88c759234ce7fdf
Minsuk Eom <boxresin@gmail.com>
2022-01-01 19:10:00 +0900

    첫 번째 커밋

---
+++ app/src/main/java/kr/boxresin/experiment/viewrecycle/MainActivity.kt
@@ -0,0 +1,13 @@
+package kr.boxresin.experiment.viewrecycle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/** 메인 액티비티 */
+class MainActivity : AppCompatActivity() {
+    /** 메인 액티비티가 생성될 때 호출된다. */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
